27f475f2b655090439f76cffd8c301cdc5633a9e
sagarmanghnani97 <sagar@garageplug.com>
2019-03-09 17:48:46 +0530

    basic card and layout created in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,6 @@ class TodoUser extends StatelessWidget {
                 Icons.account_circle,
                 size: 45.0,
                 color: Colors.white,
-                
               ),
             ),
           ),
@@ -64,10 +63,75 @@ class TodoUser extends StatelessWidget {
               style: TextStyle(fontSize: 36.0, color: Colors.white),
             ),
           ),
-          new Text("Looks Like Feel Good", style: TextStyle(color: Colors.white, fontSize: 20.0),),
-          new Text("You Have 3 tasks to do Today", style: TextStyle(color: Colors.white, fontSize: 20.0),)
+          new Text(
+            "Looks Like Feel Good",
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          ),
+          new Text(
+            "You Have 3 tasks to do Today",
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          ),
+          new CardDesign()
         ],
       ),
     );
+  }
+}
+
+class CardDesign extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(child: ListView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: 1,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, position) {
+          return GestureDetector(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: new Container(
+                  width: 250.0,
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      new Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            new Icon(Icons.account_circle),
+                            new Icon(Icons.more_vert)
+                          ],
+                        ),
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            new Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                              child: new Text("9 Tasks"),
+                            ),
+                            new Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                              child: new Text("Personal"),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: LinearProgressIndicator(value: 0.25,),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        }));
   }
 }
